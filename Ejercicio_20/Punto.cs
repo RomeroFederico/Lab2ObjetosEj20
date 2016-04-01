fc5c862eded1d7b6b8cb59c4b5f747cfca3ff6e4
RomeroFederico <romerof14@gmail.com>
2016-04-01 10:31:06 -0300

    Agregado metodos a las clases Punto y Rectangulo.

--- Ejercicio_20/Punto.cs
+++ Ejercicio_20/Punto.cs
@@ -7,5 +7,38 @@ namespace Geometria
 {
     class Punto
     {
+        private int _x;
+        private int _y;
+
+        /// <summary>
+        /// Asigna las cordenadas al nuevo punto.
+        /// </summary>
+        /// <param name="x">
+        /// Cordenada en el eje x.
+        /// </param>
+        /// <param name="y">
+        /// Cordenada en el eje y.
+        /// </param>
+        public Punto(int x, int y)
+        {
+            this._x = x;
+            this._y = y;
+        }
+
+        /// <summary>
+        /// Devuelve la cordenada en x del punto.
+        /// </summary>
+        public int GetX()
+        {
+            return this._x;
+        }
+
+        /// <summary>
+        /// Devuelve la cordenada en y del punto.
+        /// </summary>
+        public int GetY()
+        {
+            return this._y;
+        }
     }
 }
--- Ejercicio_20/Rectangulo.cs
+++ Ejercicio_20/Rectangulo.cs
@@ -6,6 +6,35 @@ using System.Text;
 namespace Geometria
 {
     class Rectangulo
-    {
+    {   
+        public float lado;
+        public float b;
+        public float area;
+        public float perimetro;
+        public Punto vertice1;
+        public Punto vertice2;
+        public Punto vertice3;
+        public Punto vertice4;
+        
+        /// <summary>
+        /// Construye un nuevo rectangulo a partir de sus vertices.
+        /// </summary>
+        /// <param name="vertice1">
+        /// Primer vertice del rectangulo.
+        /// </param>
+        /// <param name="vertice3">
+        /// Segundo vertice del rectangulo.
+        /// </param>
+        public Rectangulo(Punto vertice1, Punto vertice3)
+        {
+            this.vertice1 = vertice1;
+            this.vertice3 = vertice3;
+            this.vertice2 = new Punto(this.vertice3.GetX(), this.vertice1.GetY());
+            this.vertice4 = new Punto(this.vertice1.GetX(), this.vertice3.GetY());
+            this.lado = Math.Abs(this.vertice1.GetX() - this.vertice2.GetX());
+            this.b = Math.Abs(this.vertice4.GetY() - this.vertice1.GetY());
+            this.perimetro = this.lado * 2 + this.b * 2;
+            this.area = this.lado * this.b;
+        }
     }
 }
