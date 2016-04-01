9a59c5c4fb20729af9e5d1e2368cfcd8b7c2b302
RomeroFederico <romerof14@gmail.com>
2016-04-01 10:39:16 -0300

    Ejercicio completado.
    . Agregado metodo MostrarRectangulo en el program.
    . Agregado sumarios.
    . Agregado diagrama de clases.

--- Ejercicio_20/Program.cs
+++ Ejercicio_20/Program.cs
@@ -41,6 +41,30 @@ namespace PruebaGeometria
         static void Main(string[] args)
         {
             Console.Title = "Ejercicio Nro. 20";
+
+            Rectangulo r = new Rectangulo(new Punto(-1, -5), new Punto(-6, -1));
+
+            MostrarRectangulo(r);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Muestra los atributos del rectangulo pasado como argumento.
+        /// </summary>
+        /// <param name="rectangulo">
+        /// Rectangulo a mostrar.
+        /// </param>
+        public static void MostrarRectangulo(Rectangulo rectangulo)
+        {
+            Console.WriteLine(". Vertice 1: ({0}, {1})", rectangulo.vertice1.GetX(), rectangulo.vertice1.GetY());
+            Console.WriteLine(". Vertice 2: ({0}, {1})", rectangulo.vertice2.GetX(), rectangulo.vertice2.GetY());
+            Console.WriteLine(". Vertice 3: ({0}, {1})", rectangulo.vertice3.GetX(), rectangulo.vertice3.GetY());
+            Console.WriteLine(". Vertice 4: ({0}, {1})", rectangulo.vertice4.GetX(), rectangulo.vertice4.GetY());
+            Console.WriteLine(". Lado: {0}", rectangulo.lado);
+            Console.WriteLine(". Base: {0}", rectangulo.b);
+            Console.WriteLine(". Superficie: {0,0:#,###.00}", rectangulo.area);
+            Console.WriteLine(". Perimetro: {0,0:#,###.00}", rectangulo.perimetro);
         }
     }
 }
